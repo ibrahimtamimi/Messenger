3bdcb1305a56a177c34934e26d6d56e6df702051
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-28 12:16:24 +0300

    delete signup Function

--- server/users/userController.js
+++ server/users/userController.js
@@ -37,40 +37,6 @@ module.exports = {
 		}) 
 	},
 
-	signup: function(req, res, next){
-		var username = req.body.username;
-	    var password = req.body.password;
-	    User.findOne({username: username})
-	 		.exec(function (error, user) {
-		 		if(user){
-		 			next(new Error('User already exist!'));
-		 		}else{
-	 				var newUser = new User ({
-						username: username,
-				        password: password,
-				        firstName:req.body.firstName,
-				        lastName:req.body.lastName,
-				        city:req.body.city,
-				        country:req.body.country,
-				        rate:req.body.rate,
-				        interests:req.body.interests,
-				        picture:req.body.picture,
-				        game:req.body.game,
-				        email : req.body.email,
-				        fb_ID:req.body.fb_ID
-					})
-			 		newUser.save(function(err, newUser){
-			            if(err){
-			                res.status(500).send(err);
-			            } else {
-			              res.status(200).send(newUser);
-			            };
-			        });
-		 		}
-	 		})
-	 
-	},
-	
 	getUser: function(req, res, next){
 		User.findOne({_id: req.params.id}, function(err, user){
 			if(err) {
@@ -105,8 +71,5 @@ module.exports = {
       }
     })
 		
-	},
-
-
-	
+	},	
 }
